c693d24cb4682e2a8a74f8f9709a93e8bad14fbf
Leaves333 <canchenli@gmail.com>
2025-02-20 17:51:03 -0600

    bad sol to maximal rectangle

--- cpp/85_maximal_rectangle.cpp
+++ cpp/85_maximal_rectangle.cpp
@@ -9,8 +9,7 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
 
         int n = matrix.size(), m = matrix[0].size();
-        vvi dp_left(n, vi(m));
-        vvi dp_top(n, vi(m));
+        vvi dp(n, vi(m));
 
         for (int i = 0; i < n; i++) {
             int cur = 0;
@@ -19,54 +18,21 @@ public:
                     cur++;
                 else
                     cur = 0;
-                dp_left[i][j] = cur;
-            }
-        }
-
-        for (int j = 0; j < m; j++) {
-            int cur = 0;
-            for (int i = 0; i < n; i++) {
-                if (matrix[i][j] == '1')
-                    cur++;
-                else
-                    cur = 0;
-                dp_top[i][j] = cur;
+                dp[i][j] = cur;
             }
         }
 
         int ans = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
-                // calculate max area of rectangle with corner at i, j
-
-                // build a min stack of heights
-                stack<int> min_stack;
-                for (int k = i; k > i - dp_left[i][j]; k--) {
-                    if (min_stack.empty())
-                        min_stack.push(dp_top[k][j]);
-                    else
-                        min_stack.push(min(min_stack.top(), dp_top[k][j]));
+                int smol = INT_MAX;
+                for (int k = j; k < n; k++) {
+                    smol = min(smol, dp[k][i]);
+                    int box = (k - j + 1) * smol;
+                    ans = max(ans, (k - j + 1) * smol);
                 }
-
-                // try and go as high as possible
-                int height = 0;
-                int min_left = INT_MAX;
-                while (min_stack.size() > 0) {
-                    while (min_left >= min_stack.size() && j - height >= 0 && height < min_stack.top()) {
-                        min_left = min(min_left, dp_left[i][j - height]);
-                        height++;
-                    }
-
-                    int sz = (height - 1) * (int) min_stack.size();
-                    cout << "size at " << i << ", " << j << " could be " << sz << endl;
-                    ans = max(ans, (height - 1) * (int) min_stack.size());
-                    min_stack.pop();
-                }
-
             }
         }
-
         return ans;
 
     }
